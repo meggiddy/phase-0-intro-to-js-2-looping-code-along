826e64160cdffc9284bb6428fcec98db65bb3217
Maggie <muriithinyawira13@gmail.com>
2022-11-26 18:25:54 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -21,9 +21,8 @@ function writeCards(name, event) {
 }
 
 
-function countDown () {
-    let i = 10;
-    while (i > -1) {
+function countDown (i) {
+    while (i >=0) {
         console.log(i--);
     }
 }
